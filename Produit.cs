2a765b6e72531afe5e9d830b05921b88962dd24f
rsevrette <romain.sevrette@edouardgand.fr>
2026-03-30 15:53:06 +0200

    EXO 2

--- Produit.cs
+++ Produit.cs
@@ -15,4 +15,14 @@ class Produit {
     {
         return $"Produit {{ codeArticle={codeArticle}, designation={designation}, prix={prix} }}";
     }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Produit autre)
+        {
+            return this.codeArticle == autre.codeArticle;
+        }
+        else return false;
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -9,5 +9,10 @@ class Program
         Produit p1 = new Produit("0123", "azerty", 15);
         Console.WriteLine(p1.ToString());
 
+        //EXERCICE 2
+        Console.WriteLine("\n____________________EXO 2____________________");
+        Produit p2 = new Produit("A01", "souris", 15.0);
+        Produit p3 = new Produit("A01", "souris", 15.0);
+        Console.WriteLine(p2.Equals(p3));
     }
 }
